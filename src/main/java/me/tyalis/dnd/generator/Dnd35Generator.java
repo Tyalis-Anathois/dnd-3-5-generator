ffca4829fcb10a19dfc2dc23160b1f814e2c50b8
ToOcMGit <to_oc_m_git@outlook.be>
2024-08-08 23:28:56 +0200

    Created Maven project

---
+++ src/main/java/me/tyalis/dnd/generator/Dnd35Generator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package me.tyalis.dnd.generator;
+
+/**
+ *
+ * @author User
+ */
+public class Dnd35Generator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
